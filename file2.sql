946bad4f806824552142fd3c4db7ddfce0498c41
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2025-02-05 22:48:29 +0530

    added files

--- file2.sql
+++ file2.sql
@@ -144,4 +144,4 @@ VALUES
 ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
 ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
 ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
-('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, );
